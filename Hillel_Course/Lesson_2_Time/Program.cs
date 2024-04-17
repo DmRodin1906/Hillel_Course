d4160968b69b635466b1b88002c9099276ddc63d
DmRodin1906 <dmitriyarodin@gmail.com>
2024-04-17 21:02:03 +0300

    add new project

---
+++ Hillel_Course/Lesson_2_Time/Program.cs
@@ -0,0 +1,7 @@
+﻿int hours = 4;
+int minutes = 35;
+int seconds = 48;
+
+int time_seconds = hours * 60 + minutes * 60 + seconds;
+Console.WriteLine($"overall time in seconds = {time_seconds}");
+Console.ReadKey();
